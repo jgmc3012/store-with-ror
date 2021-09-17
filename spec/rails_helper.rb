3bd769504a199d1e7bf41e2a24d2be0dee7eaf62
Jesus Millan <millan@asistesi.com>
2021-09-17 00:34:10 -0400

    feat: Set up Data_cleaner, factory_bot and shoulda_matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,14 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+# Set up shoulda matchers -> https://github.com/thoughtbot/shoulda-matchers#rspec
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -30,9 +38,21 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-RSpec.configure do |config|
+RSpec.configure do |config|  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # Include FactoryBot methods ->
+  config.include FactoryBot::Syntax::Methods
+  # Include helpers ->
+  config.include ApplicationHelper
+  
+  # Database cleaner set up -> https://github.com/DatabaseCleaner/database_cleaner#rspec-example
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
