9ddc91f2df2bbeefb5ffb56cd858bba293135a04
Jesús Millán <jgmc3012@gmail.com>
2021-11-06 22:01:28 -0400

    feat: Test for update product

---
+++ spec/controllers/products/update_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe V1::ProductsController, type: :controller do
+  describe 'Update Product' do
+    let(:user) { create(:owner) }
+    let(:token) { create(:token, user: user) }
+    let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
+    let(:product) { create(:product, store: user.store) }
+
+    context 'update success' do
+      before do
+        request.headers.merge!(headers)
+        put :update, format: :json, params: { id: product.id, product: { name: 'new name' } }
+      end
+      context 'response with status 200' do
+        subject { response } 
+        it { expect(response).to have_http_status(200) }
+      end
+      context 'response with correct format product' do
+        subject { payload_test }
+        it { is_expected.to include(:id, :price, :description, :name, :store_id) }
+      end
+    end
+    
+    context 'update fail' do
+      before do
+        request.headers.merge!(headers)
+        put :update, format: :json, params: { id: product.id, product: { name: nil } }
+      end
+      context 'response with status 400' do
+        subject { response } 
+        it { expect(response).to have_http_status(:bad_request) }
+      end
+      context 'response with correct format error' do
+        subject { payload_test }
+        it { is_expected.to include(:errors) }
+      end
+    end
+
+    context 'update without token' do
+      before do
+        put :update, format: :json, params: { id: product.id, product: { name: 'new name' } }
+      end
+      context 'response with status 401' do
+        subject { response } 
+        it { expect(response).to have_http_status(:unauthorized) }
+      end
+    end
+  end
+end
